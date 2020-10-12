b2f6e6062bdad2506bdd2a81160ec59db11217e1
Xalvetra <farisikhlasulhaq@yahoo.co.id>
2020-10-12 19:24:30 +0700

    Update WEB 🛩

---
+++ PHP1/operator4.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<?php
+	$x = 4;
+	$x1 = 4;
+	$x2 = 4;
+	$x3 = 4;
+	$x4 = 4;
+	$x5 = 4;
+	$x += 3;
+	$x1 -= 3;
+	$x2 *= 3;
+	$x3 /= 3;
+	$x4 %= 3;
+	$x5 .= 3;
+	echo "hasil operasi tersebut adalah =$x";
+	echo "<br>A. hasil operasi (-) tersebut adalah =$x1"; 
+	echo "<br>B. hasil operasi (*) tersebut adalah =$x2"; 
+	echo "<br>C. hasil operasi (/) tersebut adalah =$x3"; 
+	echo "<br>D. hasil operasi (%) tersebut adalah =$x4";
+	echo "<br>E. hasil operasi (.) tersebut adalah =$x5"; 
+	?>
+</body>
+</html>
